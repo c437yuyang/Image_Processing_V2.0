094563270f756aaaca1946b1cc17c04dfabab01d
YXP <954222619@qq.com>
2017-07-04 19:38:46 +0800

    添加文件

---
+++ Image_Processing/Image_ProcessingView.h
@@ -0,0 +1,77 @@
+
+// Image_ProcessingView.h : CImage_ProcessingView 类的接口
+//
+
+#pragma once
+#include "MyImage_.h"
+
+class CImage_ProcessingView : public CScrollView
+{
+protected: // 仅从序列化创建
+	CImage_ProcessingView();
+	DECLARE_DYNCREATE(CImage_ProcessingView)
+
+// 特性
+public:
+	CImage_ProcessingDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void OnInitialUpdate(); // 构造后第一次调用
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CImage_ProcessingView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnFileOpen();
+public:
+	MyImage_ m_Image,m_ImageAfter;
+	BOOL m_bIsProcessed;
+	BOOL m_bIsGrayed;
+	UINT m_nResDownRate;
+	UINT m_nGrayRate;
+	afx_msg void OnDrawline();
+	afx_msg void OnFileSaveAs();
+	afx_msg void OnShowred();
+//	afx_msg void OnViewToolbar();
+	afx_msg void OnDoubleimage();
+private:
+	// save the original file's name
+	CString m_strFileNameSave;
+public:
+	afx_msg void OnReverse();
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnTogray();
+	afx_msg void OnResdwon();
+	afx_msg void OnRetrieve();
+	afx_msg void OnTest();
+	afx_msg void OnGraytohalf();
+};
+
+#ifndef _DEBUG  // Image_ProcessingView.cpp 中的调试版本
+inline CImage_ProcessingDoc* CImage_ProcessingView::GetDocument() const
+   { return reinterpret_cast<CImage_ProcessingDoc*>(m_pDocument); }
+#endif
+
